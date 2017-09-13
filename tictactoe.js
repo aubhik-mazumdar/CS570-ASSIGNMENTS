0c55f52d172c8334055b0cbcdad30fcdc0b79ccb
Aubhik Mazumdar <aubhik.mazumdar@gmail.com>
2017-09-12 23:28:23 -0400

    added checkWin(not complete)

--- tictactoe.js
+++ tictactoe.js
@@ -9,8 +9,63 @@ function game(p,bs,w,arr){
 	this.array = arr;
 }
 
-function saveGame(game){
+function displayBoard(){
+/* -----
+	-----PRINT BOARD WITH SYMBOLS IN RIGHT PLACES
+	----
+*/
+}
 
+function arraysearch(arr1,arr2){
+	let i=0,flag=0;
+	[a1,a2,a3]= arr1;
+	for(i in arr2){
+		[b1,b2,b3]=arr2[i];
+		if(a1==b1 && a2==b2 && a3==b3){
+			flag=1;
+		}
+	}
+	return flag;
+}
+
+function checkWin(lastmove,moves,win,bs){
+	let rn=0,cn=0,s='';
+	[rn,cn,s]  = lastmove;
+	let wincount=1,i=0;
+	for (let i = 0; i < bs; i++){
+
+	}
+}
+
+function checkCell(game){
+
+}
+
+function checkMove(gameObject,turn){
+	displayBoard();
+	moves = gameObject["array"];
+	console.log(moves);
+	let lastMove = moves[moves.length-1];
+	console.log(lastMove);
+	let r=0;c=0;
+	return checkWin(lastMove,moves,gameObject['win_sequence'],gameObject['board_size']);
+}
+
+function startGame(p,bs,w){
+	let symbols="XOABCDEFGHIJKLMNPQRSTUVWYZ",s='';
+	let arr=[],turn=0;
+	let ngame= new game(p,bs,w,arr);
+	console.log(ngame); 
+	printboard(bs);
+	while(move=readline.question("Enter your move player "+ (turn % p) +" , row and column:")){
+		[row,column]=move.split(" ");
+		s=symbols.charAt(turn % p);
+		ngame["array"].push([row,column,s]);
+		//console.log(ngame);
+		//process.stdout.write(row.toString() + " " + column.toString() + " " + s);
+		checkMove(ngame,turn);
+		turn+=1;
+	}
 }
 
 function printboard(board_size){
@@ -18,7 +73,7 @@ function printboard(board_size){
 	process.stdout.write(" ");
 	for(i=1;i<=board_size;i++){
 		process.stdout.write(" ");
-		process.stdout.write(i.toString())
+		process.stdout.write(i.toString());
 		process.stdout.write("  ");
 	}
 	process.stdout.write("\n");
@@ -76,7 +131,6 @@ function newGame(){
 	players = readline.question("Enter the number of players(minimum 2, maximum 26):");
 	board_size= readline.question("Enter the size of the board(minimum 3, maximum 999:");
 	win_sequence= readline.question("Enter the winning sequence count:");
-	console.log(players,board_size,win_sequence);
 	if(win_sequence<3){
 		console.error("ERROR- Please enter a winning sequence count > 2");
 		Error;
@@ -87,8 +141,7 @@ function newGame(){
 	}
 	else
 	{
-		printboard(board_size);
-		startGame();
+		startGame(parseInt(players),parseInt(board_size),parseInt(win_sequence));
 	}
 	//new newGame= game(players,board_size,win_sequence,array) 	
 }
