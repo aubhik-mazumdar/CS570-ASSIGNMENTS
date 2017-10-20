984992e0f2c996472f3ca0075d9bded956b08af0
Aubhik Mazumdar <aubhik.mazumdar@gmail.com>
2017-10-20 18:52:44 -0400

    Update huffman.js
    taking input from a file

--- HuffmanCoding/huffman.js
+++ HuffmanCoding/huffman.js
@@ -1,3 +1,18 @@
-let infile='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasssssssssssssssssssssssssrrrrrrrrrrrraaaaaaaaaaaaassssssssssssssrrrrrreeeee
-			eeeeeeeennnnnnnnnnnnnnnnsssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaannnnnnnnnnnnnnnnnnnnnssssssssssaaaaaaaaaaannnnneneneneneeneenenaaaaaasssssssss';
-console.log(infile.length);
+const fs = require('fs');
+const readline = require('readline-sync');
+
+
+let infile = fs.readFileSync('infile.dat', 'UTF-8');
+
+//string of length = 250
+let record = new Map();
+for(let c of infile){
+	c = c.toLowerCase();
+	if(c.match(/[a-z]/)){
+		if(!record.get(c))
+			record.set(c,1);
+		else
+			record.set(c,record.get(c)+1);
+	}
+}
+console.log(record);
